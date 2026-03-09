d4f48bd6f87f1c175fb1d1c28b1e250ef9dd9951
danbi <danbi681591@gmail.com>
2026-03-09 17:42:55 +0900

    03/09 - 최종 UI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,14 +21,11 @@ export default defineNuxtConfig({
         '~/plugins/axios.js'
     ],
     // 환경 변수 설정
-    /*
     runtimeConfig: {
         public: {
-            apiBase: process.env.NUXT_PUBLIC_API_BASE || '/api'
+            apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:3001'
         }
     },
-
-     */
     devServer: {
         port: 3020,   // 원하는 포트
         host: '0.0.0.0'  // 선택: 로컬 LAN 접속 허용
